ee994006977005cca45d5e0be3f3ea1f18ce6e1a
Julien Anest <julien.anest@evogue.fr>
2020-03-13 17:00:53 +0100

    Commentaires

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -55,9 +57,17 @@ class Article
      */
     private $image;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article")
+     * tri par défaut quand on appelle le getter :
+     * @ORM\OrderBy({"publicationDate": "DESC"})
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->setPublicationDate(new \DateTime());
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -140,6 +150,37 @@ class Article
     public function setImage($image)
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
 
         return $this;
     }
